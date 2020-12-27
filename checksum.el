d4b7478e66e20d8b1174b758a0695f77f87a0ca3
Jackson Benete <jacksonbenete@gmail.com>
2020-12-26 23:48:53 -0300

    Feature: checksum-dired, fix #1

--- checksum.el
+++ checksum.el
@@ -43,7 +43,34 @@
 
 (defgroup checksum nil
   "Compare hash of a file."
-  :group 'emacs)
+  :group 'applications)
+
+(defvar checksum-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "q") 'quit-window)
+    (define-key map (kbd "Q") 'kill-this-buffer)
+    map)
+  "Keymap for interacting with checksum application buffer, since it will be 
+a read-only buffer.")
+
+(define-derived-mode checksum-mode special-mode "Checksum"
+  "Major mode for controlling process output buffer from checksum-cli.
+\\<checksum-mode-map>"
+  :group 'checksum
+  :syntax-table nil
+  :abbrev-table nil
+  (buffer-disable-undo)
+  (whitespace-mode -1)
+  (linum-mode -1)
+  (when (>= emacs-major-version 26)
+    (display-line-numbers-mode -1))
+  (page-break-lines-mode 1)
+  (setq inhibit-startup-screen t
+        buffer-read-only t
+        truncate-lines t))
+
+(defvar checksum-buffer-name "*Process: checksum-cli*"
+  "Define a common buffer name to handle process call.")
 
 (defvar checksum-select-hash-list
   '(("ADLER32" . 'ADLER32) ("BLAKE2" . 'BLAKE2) ("BLAKE2/160" . 'BLAKE2/160)
@@ -73,6 +100,21 @@
     "Return a list of digests supported by ironclad.
 See (ironclad:list-all-digests) for an updated list.")
 
+(defvar checksum-supported-hash-list
+  '("ADLER32" "BLAKE2" "BLAKE2/160" "BLAKE2/256" "BLAKE2/384" "BLAKE2S"
+    "BLAKE2S/128" "BLAKE2S/160" "BLAKE2S/224" "CRC24" "CRC32" "GROESTL"
+    "GROESTL/224" "GROESTL/256" "GROESTL/384" "JH" "JH/224" "JH/256" "JH/384"
+    "KECCAK" "KECCAK/224" "KECCAK/256" "KECCAK/384" "KUPYNA" "KUPYNA/256" "MD2"
+    "MD4" "MD5" "RIPEMD-128" "RIPEMD-160" "SHA1" "SHA224" "SHA256" "SHA3"
+    "SHA3/224" "SHA3/256" "SHA3/384" "SHA384" "SHA512" "SHAKE128" "SHAKE256"
+    "SKEIN1024" "SKEIN1024/384" "SKEIN1024/512" "SKEIN256" "SKEIN256/128"
+    "SKEIN256/160" "SKEIN256/224" "SKEIN512" "SKEIN512/128" "SKEIN512/160"
+    "SKEIN512/224" "SKEIN512/256" "SKEIN512/384" "SM3" "STREEBOG"
+    "STREEBOG/256" "TIGER" "TREE-HASH" "WHIRLPOOL")
+  "Return a list of digests supported by ironclad.
+This is useful for checking if a file type is member of.
+See (ironclad:list-all-digests) for an updated list.")
+
 ;;; TODO: this function isn't working yet.
 ;;; The function should "read-file-name" showing only supported
 ;;; filenames.
@@ -84,7 +126,7 @@ See (ironclad:list-all-digests) for an updated list.")
    (read-file-name "Select hash: "
 		   nil nil nil nil
 		   (lambda (x) (member (intern (file-name-extension x))
-				       (secure-hash-algorithms))))))
+				       checksum-supported-hash-list)))))
 
 (defun checksum-find-hash ()
   "Find the hash file."
@@ -103,30 +145,114 @@ http://ergoemacs.org/emacs/elisp_read_file_content.html"
     (insert-file-contents file-path)
     (buffer-string)))
 
+(defun checksum-cli-compare-hashes (file hash hash-type)
+  "Send a file and a hash to checksum-cli to compare hashes.
+Return results as a read-only help-like buffer."
+  (set-process-sentinel
+     (start-process
+      "checksum-cli" checksum-buffer-name
+      "~/.emacs.d/emacs-checksum/checksum-cli"
+      "--operation" "compare-hash"
+      "--spec" (symbol-name hash-type)
+      "--file" file
+      "--hash" hash)
+     (lambda (p e) (when (= 0 (process-exit-status p))
+		     (pop-to-buffer checksum-buffer-name
+				    (with-current-buffer checksum-buffer-name
+				      (beginning-of-buffer)
+				      (checksum-mode)))))))
+
+(defun checksum-cli-generate-hash (file)
+  (let* ((key (completing-read
+	       "Select hash type: " checksum-select-hash-list))
+	 (val (alist-get key checksum-select-hash-list nil nil #'string=))
+	 (hash-type (cadr val)))
+    (set-process-sentinel
+     (start-process
+      "checksum-cli" checksum-buffer-name
+      "~/.emacs.d/emacs-checksum/checksum-cli"
+      "--operation" "generate-hash"
+      "--spec" (symbol-name hash-type)
+      "--file" file
+      "--hash" hash)
+     (lambda (p e) (when (= 0 (process-exit-status p))
+		     (pop-to-buffer checksum-buffer-name
+				    (with-current-buffer checksum-buffer-name
+				      (beginning-of-buffer)
+				      (checksum-mode))))))))
+
+(defun checksum-select-hash ()
+  "Select hash from list and return as symbol."
+  (let* ((key (completing-read
+	       "Select hash type: " checksum-select-hash-list))
+	 (val (alist-get key checksum-select-hash-list nil nil #'string=))
+	 (hash-type (cadr val)))
+    hash-type))
+
+(defun checksum-dired-compare-files (file-list)
+  "Receive two files, an object and a hash.
+Generate a hash for the object and compare hashes.
+A file ending with .txt or .spec will be treated as hash."
+  (let* ((first-file (car file-list))
+	 (second-file (cadr file-list))
+	 (first-file-type (upcase (file-name-extension first-file)))
+	 (second-file-type (upcase (file-name-extension second-file)))
+	 (first-fake-txt (upcase (file-name-extension (substring first-file 0 -4))))
+	 (second-fake-txt (upcase (file-name-extension (substring second-file 0 -4)))))
+    ;; If file type is supported (e.g. md5) call the function
+    ;; If file type is txt, remove txt and test again,
+    ;; If the file was <file>.md5.txt then md5 will be used,
+    ;; Else, ask for the correct spec.
+    (cond ((member first-file-type checksum-supported-hash-list)
+	   (checksum-cli-compare-hashes second-file first-file (intern first-file-type)))
+	  
+	  ((string= first-file-type "TXT")
+	   (if (member first-fake-txt checksum-supported-hash-list)
+	       (checksum-cli-compare-hashes second-file first-file (intern first-fake-txt))
+	     (checksum-cli-compare-hashes second-file first-file (checksum-select-hash))))
+
+	  ((member second-file-type checksum-supported-hash-list)
+	   (checksum-cli-compare-hashes first-file second-file (intern second-file-type)))
+	  
+	  ((string= second-file-type "TXT")
+	   (if (member second-fake-txt checksum-supported-hash-list)
+	       (checksum-cli-compare-hashes first-file second-file (intern second-fake-txt))
+	     (checksum-cli-compare-hashes first-file second-file (checksum-select-hash))))
+	  
+	  (t (message "Error: Hash file must end as *.spec or *.txt, 
+	  where `'spec` is one of the supported types.'")))))
+
 (defun checksum ()
   "Compare hash between two files and show results in a help buffer."
   (interactive)
   ;; Get hash, or ask for the hash type if selected file doesn't have a type.
   (setq hash (checksum-find-hash))
+  (setq hash-type-string (upcase (file-name-extension hash)))
   (setq hash-type (intern (file-name-extension hash)))
-  (unless (member hash-type (secure-hash-algorithms))
+  (unless (member hash-type-string checksum-supported-hash-list)
     (let* ((key (completing-read
 		 "Select hash type: " checksum-select-hash-list))
 	   (val (alist-get key checksum-select-hash-list nil nil #'string=)))
       (setq hash-type (cadr val))))
   ;; Get the file object to be compared
   (setq file (checksum-find-file))
-  (let ((cli-buffer-name "*Process: checksum-cli*"))
-    (set-process-sentinel
-     (start-process
-      "checksum-cli" cli-buffer-name
-      "~/.emacs.d/emacs-checksum/checksum-cli"
-      "--spec" (symbol-name hash-type)
-      "--file" file
-      "--hash" hash)
-     (lambda (p e) (when (= 0 (process-exit-status p))
-		     (pop-to-buffer cli-buffer-name
-				    (beginning-of-buffer)))))))
+  (checksum-cli-compare-hashes file hash hash-type))
+
+;;; TODO: handle multiple files
+(defun checksum-dired ()
+  "Compare or generate hash between files using dired."
+  (interactive)
+  (let* ((file-list (dired-get-marked-files))
+	 (file-list-length (length file-list)))
+    (if (eq major-mode 'dired-mode)
+	(cond ((= file-list-length 1)
+	       (checksum-cli-generate-hash (car file-list)))
+	      ((= file-list-length 2)
+	       (checksum-dired-compare-files file-list))
+	      ((> file-list-length 2)
+	       (message "Error: Multiple files not supported yet."))
+	      (t (message "Error: There is no file selected.")))
+      (message "Error: You're not in dired-mode."))))
 
 (provide 'checksum)
 ;;; end of file checksum.el
